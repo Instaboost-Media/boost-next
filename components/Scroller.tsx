c32aec91aa26c864c4d2c3e4e1d9105415c9ee44
Alexis Martin <alexismartin1895@gmail.com>
2023-12-19 14:42:49 -0800

    added scroller

--- components/Scroller.tsx
+++ components/Scroller.tsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { montserrat } from "../app/fonts";
 
 const Scroller = () => {
 	return (
-		<div className="h-14 bg-boost-tan-1  border-y-[2px] border-boost-black-2">
-			Scroller
+		<div className="h-14 bg-boost-tan-1  border-y-[2px] border-boost-black-2 overflow-hidden flex items-center p-4">
+			<p className={`slider-bar whitespace-nowrap ${montserrat.className}`}>
+				Growth strategy & technology for e-commerce companies. Growth strategy &
+				technology for e-commerce companies. Growth strategy & technology for
+				e-commerce companies. Growth strategy & technology for e-commerce
+				companies. Growth strategy & technology for e-commerce companies. Growth
+				strategy & technology for e-commerce companies. Growth strategy &
+				technology for e-commerce companies. Growth strategy & technology for
+				e-commerce companies. Growth strategy & technology for e-commerce
+				companies. Growth strategy & technology for e-commerce companies.
+			</p>
 		</div>
 	);
 };
