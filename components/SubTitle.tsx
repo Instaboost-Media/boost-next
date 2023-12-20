1f213b881760df94db1f690ffd0fba982fc5544c
Alexis Martin <alexismartin1895@gmail.com>
2023-12-20 13:59:11 -0800

    made why boost section responsive

--- components/SubTitle.tsx
+++ components/SubTitle.tsx
@@ -3,11 +3,13 @@ import Image from "next/image";
 
 type SubTitleProps = {
 	children: ReactNode;
+	styles?: string;
 };
 
-const SubTitle: FC<SubTitleProps> = ({ children }) => {
+const SubTitle: FC<SubTitleProps> = ({ children, styles }) => {
 	return (
-		<h2 className="text-3xl xs:text-4xl text-boost-black-1 text-center">
+		<h2
+			className={`text-3xl xs:text-4xl text-boost-black-1 text-center ${styles}`}>
 			{children}
 		</h2>
 	);
--- components/WhyBoost.tsx
+++ components/WhyBoost.tsx
@@ -8,43 +8,49 @@ const WhyBoost = () => {
 	return (
 		<div className="mt-36 ">
 			<Title>Why InstaBoost?</Title>
-			<div className="flex flex-col justify-center items-center gap-6 mt-20">
+			<div className="flex flex-col justify-center items-center gap-6 mt-20 lg:gap-28 lg:flex-row-reverse">
 				<Image
 					src="/placeholder.webp"
 					width={500}
 					height={500}
 					alt="instaboost logo"
-					className="banner-visual border-[2px] border-boost-black-1"
+					className="arch-img border-[2px] border-boost-black-1"
 				/>
-				<SubTitle>Lorem ipsum</SubTitle>
-				<p
-					className={` text-center font-normal text-lg text-boost-black-1 max-w-[450px] ${montserrat.className} `}>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae illum
-					dolor necessitatibus. Ea, sed voluptate?
-				</p>
+				<div className="flex flex-col justify-center gap-6">
+					<SubTitle styles={"lg:text-left"}>Lorem ipsum</SubTitle>
+					<p
+						className={` text-center font-normal text-lg text-boost-black-1 max-w-[450px] lg:max-w-[600px] lg:text-left ${montserrat.className} `}>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+						accusantium maxime quas, voluptates molestias possimus ab magnam
+						pariatur! Nihil earum sapiente vel.
+					</p>
+				</div>
 			</div>
 			<div className="flex flex-col justify-center items-center gap-6 mt-20">
 				<SubTitle>Our Track Record</SubTitle>
 				<p
-					className={` text-center font-normal text-lg text-boost-black-1 max-w-[450px] ${montserrat.className} `}>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae illum
-					dolor necessitatibus. Ea, sed voluptate?
+					className={` text-center font-normal text-lg text-boost-black-1 max-w-[450px] lg:max-w-[750px] ${montserrat.className} `}>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+					accusantium maxime quas, voluptates molestias possimus ab magnam
+					pariatur! Nihil earum sapiente vel.
 				</p>
 			</div>
 			<div className="flex flex-col justify-center items-center gap-6 mt-20 ">
-				<SubTitle>Our Track Software</SubTitle>
+				<SubTitle>Our Software</SubTitle>
 				<p
-					className={` text-center font-normal text-lg text-boost-black-1 max-w-[450px] ${montserrat.className} `}>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae illum
-					dolor necessitatibus. Ea, sed voluptate?
+					className={` text-center font-normal text-lg text-boost-black-1 max-w-[450px] lg:max-w-[750px] ${montserrat.className} `}>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+					accusantium maxime quas, voluptates molestias possimus ab magnam
+					pariatur! Nihil earum sapiente vel.
 				</p>
 			</div>
 			<div className="flex flex-col justify-center items-center gap-6 mt-20">
 				<SubTitle>Our Creative</SubTitle>
 				<p
-					className={` text-center font-normal text-lg text-boost-black-1 max-w-[450px] ${montserrat.className} `}>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae illum
-					dolor necessitatibus. Ea, sed voluptate?
+					className={` text-center font-normal text-lg text-boost-black-1 max-w-[450px] lg:max-w-[750px] ${montserrat.className} `}>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+					accusantium maxime quas, voluptates molestias possimus ab magnam
+					pariatur! Nihil earum sapiente vel.
 				</p>
 			</div>
 		</div>
