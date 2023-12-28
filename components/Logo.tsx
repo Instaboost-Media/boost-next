9b9f0002d1ccbe8d8cb1df975660b6f4598f91fe
Alexis Martin <alexismartin1895@gmail.com>
2023-12-28 11:55:11 -0800

    changed logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -4,11 +4,11 @@ import Image from "next/image";
 const Logo = () => {
 	return (
 		<Image
-			src="/boost-logo.svg"
+			src="/boost-brands-logo.svg"
 			width={0}
 			height={0}
 			alt="instaboost logo"
-			className="w-24"
+			className="w-44 flex"
 		/>
 	);
 };
