22ef5fed3a96d8af13656ceddcecddc06a16f8cf
Alexis Martin <webdeveloperlex@gmail.com>
2024-01-11 11:02:50 -0800

    removed bg colors

--- components/Popup.tsx
+++ components/Popup.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 const Popup = ({ handlePopup, openPopup}) => {
   return (
     <div onClick={handlePopup} className={`z-40 fixed w-full h-full left-0 top-0 bg-black bg-opacity-75 flex justify-center items-center overflow-auto ${openPopup ? "" : "pointer-events-none opacity-0"}`}>
-      <div className='w-full top-10 md:w-[650px] h-[775px] absolute md:top-20 bg-green-500'>
+      <div className='w-full top-10 md:w-[650px] h-[775px] absolute md:top-20'>
       <button type="button" className='absolute left-7 top-7 z-50'>
         <Image
               src="/xmark-solid.svg"
@@ -17,7 +17,7 @@ const Popup = ({ handlePopup, openPopup}) => {
       </button>
       <iframe
         src="https://link.instaboostinc.com/widget/form/MjufEOKIcIATadF1aq20"
-        className='!w-full h-full rounded-md absolute bg-red-500' 
+        className='!w-full h-full rounded-md absolute' 
         id="inline-MjufEOKIcIATadF1aq20" 
         data-layout="{'id':'INLINE'}"
         data-trigger-type="alwaysShow"
